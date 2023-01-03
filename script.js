94f5e8b2ee2d0d8b79f96d8c2a8b43847e2a6675
Jacek Wójciak <fr.banka@gmail.com>
2023-01-03 20:53:05 +0100

    base

---
+++ script.js
@@ -0,0 +1,5 @@
+const enterButton = document.querySelector(".button");
+enterButton.addEventListener("click", enterPageTwo);
+function enterPageTwo() {
+  window.location.href = "./page-two.html";
+}
